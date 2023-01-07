351d53b41bccf4b96f3c216d93d71a9ffc663e46
wonseok <nsk7640@naver.com>
2023-01-07 12:07:31 +0900

    UserLoanHistoryResponse 의 정적팩토리 메소리를 활용한 리펙토링

--- src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
+++ src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
@@ -1,11 +1,33 @@
 package com.group.libraryapp.dto.user.response
 
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
+
 data class UserLoanHistoryResponse(
     val name: String, //유저 이름
     val books: List<BookHistoryResponse>
-)
+) {
+    companion object {
+        fun of(user: User): UserLoanHistoryResponse {
+            return UserLoanHistoryResponse(
+                name = user.name,
+                books = user.userLoanHistories.map(BookHistoryResponse::of)
+            )
+        }
+    }
+}
 
 data class BookHistoryResponse(
-    val name: String , //책의 이름
+    val name: String, //책의 이름
     val isReturn: Boolean //반납 여부
-)
+) {
+    companion object {
+        fun of(history: UserLoanHistory): BookHistoryResponse {
+            return BookHistoryResponse(
+                name = history.bookName,
+                isReturn = history.isReturn //커스텀 getter 를 이용하여 로직처리 코드 정리
+            )
+        }
+    }
+}
