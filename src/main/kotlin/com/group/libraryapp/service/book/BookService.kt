485ddd92d8902d4e24833566d2434c38c0d2249f
wonseok <nsk7640@naver.com>
2023-01-07 20:02:52 +0900

    책의 분야별 통계 및 대출 권수 API 작성

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -8,9 +8,11 @@ import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.GetMapping
 
 @Service
 class BookService(
@@ -41,4 +43,37 @@ class BookService(
         val user = userRepository.findByName(request.userName) ?: fail()
         user.returnBook(request.bookName)
     }
+
+    //현재 대출중인 책의 개수
+    @Transactional(readOnly = true)
+    fun countLoanedBook(): Int {
+        return userLoanHistoryRepository.findAllByStatus(UserLoanStatus.LOANED).size
+    }
+
+    //분야별 책 권수 통계
+    @Transactional(readOnly = true)
+    fun getBookStatistics(): List<BookStatResponse> {
+        val results = mutableListOf<BookStatResponse>() // 반환할 빈 리스트 미리 만들어두기
+        val books = bookRepository.findAll() // 등록되어 있는 책 모두 가져오기
+
+        //책 리스트 루프 돌리며 통계 리스트 데이터 만들기
+//        for (book in books) {
+//          val targetResult = results.firstOrNull { dto -> book.type == dto.type } //통계 리스트안에 해당하는 분야가 있는지 체크
+//            if (targetResult == null) { //없으면 분야 넣어주기(분야 , 개수 1개)
+//                results.add(BookStatResponse(book.type, 1))
+//            } else {
+//                targetResult.plusOne() // 있다면 개수 1개 늘려주기
+//            }
+//        }
+
+        // 코틀린 문법을 이용하여 리펙토링 해보기
+        for (book in books) {
+            results.firstOrNull { dto -> book.type == dto.type }?.plusOne() // "firstOrNull" 결과가 null 이 아닐경우 만 plusOne() 함수 실행
+                ?: results.add(BookStatResponse(book.type, 1)) //앨리스 연산자를 이용하여 앞에 결과가 null일 경우 results.add 실행 하여 통계 데이터 넣기
+        }
+
+        return results
+    }
+
+
 }
